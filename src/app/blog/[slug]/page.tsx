6a8c27f9d88a126495076a2415e066bce19f390e
matsutomo7010 <matsutomo7010@gmail.com>
2025-07-06 14:27:57 +0900

    Fix for Next.js 15

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -2,8 +2,15 @@ import { getPostData } from '@/lib/posts';
 import { notFound } from 'next/navigation';
 import fs from 'fs';
 import path from 'path';
+import { Metadata } from 'next';
 
-export async function generateStaticParams(): Promise<{ slug: string }[]> {
+type PageProps = {
+    params: {
+        slug: string;
+    };
+};
+
+export async function generateStaticParams() {
     const postsDirectory = path.join(process.cwd(), '_posts');
     const fileNames = fs.readdirSync(postsDirectory);
     return fileNames.map((fileName) => ({
@@ -11,13 +18,22 @@ export async function generateStaticParams(): Promise<{ slug: string }[]> {
     }));
 }
 
-export default async function Post({ params }: { params: { slug: string } }) {
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+    const postData = await getPostData(params.slug);
+    return {
+        title: postData.title,
+    };
+}
+
+export default async function Post({ params }: PageProps) {
     try {
         const postData = await getPostData(params.slug);
         return (
             <article style={{ padding: '2rem' }}>
                 <h1 style={{ fontSize: '2rem' }}>{postData.title}</h1>
-                <div style={{ color: 'gray', marginBottom: '1.5rem' }}>{postData.date}</div>
+                <div style={{ color: 'gray', marginBottom: '1.5rem' }}>
+                    {postData.date}
+                </div>
                 <div
                     dangerouslySetInnerHTML={{ __html: postData.contentHtml }}
                     style={{ lineHeight: '1.7' }}
